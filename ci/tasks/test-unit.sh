025a65eb9d2d6f76e3b7eb5a60a60dfe9e6394ac
Anthony Emengo <aemengo@pivotal.io>
2016-02-23 12:25:27 -0500

    Remove debugging statements
    Signed-off-by: Charlie Vieth <cviethjr@pivotal.io>

--- ci/tasks/test-unit.sh
+++ ci/tasks/test-unit.sh
@@ -6,9 +6,6 @@ export GOPATH=$(pwd)/gopath
 export PATH=/usr/local/go/bin:$GOPATH/bin:$PATH
 export GO15VENDOREXPERIMENT=1
 
-go env
-go version
-
 cd gopath/src/github.com/cloudfoundry/bosh-utils
 go install ./vendor/github.com/onsi/ginkgo/ginkgo
 bin/test-unit
