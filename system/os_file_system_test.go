ec1ba2c769f11df034013849dfae7256447bce55
Mario Manno <mario.manno@gmail.com>
2017-03-21 16:43:51 -0700

    Move chown platform specific tests to not run on windows
    [#136927903](https://www.pivotaltracker.com/story/show/136927903)
    
    Signed-off-by: Dennis Leon <dleon@pivotal.io>

--- system/os_file_system_test.go
+++ system/os_file_system_test.go
@@ -20,8 +20,6 @@ import (
 	. "github.com/cloudfoundry/bosh-utils/assert"
 	boshlog "github.com/cloudfoundry/bosh-utils/logger"
 	. "github.com/cloudfoundry/bosh-utils/system"
-	"runtime"
-	"syscall"
 )
 
 func createOsFs() (fs FileSystem) {
@@ -105,92 +103,6 @@ var _ = Describe("OS FileSystem", func() {
 
 		err = osFs.MkdirAll(testPath, fileMode)
 		Expect(err).ToNot(HaveOccurred())
-	})
-
-	Describe("chown", func() {
-		var testPath string
-		BeforeEach(func() {
-			testPath = filepath.Join(os.TempDir(), "ChownTestDir")
-
-			err := os.Mkdir(testPath, os.FileMode(0700))
-			Expect(err).ToNot(HaveOccurred())
-		})
-
-		AfterEach(func() {
-			defer os.RemoveAll(testPath)
-		})
-
-		if runtime.GOOS == "linux" {
-			It("should chown file with owner:group syntax", func() {
-				osFs := createOsFs()
-
-				err := os.Chown(testPath, 1000, 1000)
-				Expect(err).ToNot(HaveOccurred())
-
-				err = osFs.Chown(testPath, "root:root")
-				Expect(err).ToNot(HaveOccurred())
-				testPathStat, err := osFs.Stat(testPath)
-				Expect(err).ToNot(HaveOccurred())
-
-				Expect(testPathStat.Sys().(*syscall.Stat_t).Uid).To(Equal(uint32(0)))
-				Expect(testPathStat.Sys().(*syscall.Stat_t).Gid).To(Equal(uint32(0)))
-			})
-
-			It("should chown file with owner syntax", func() {
-				osFs := createOsFs()
-
-				err := os.Chown(testPath, 1000, 1000)
-				Expect(err).ToNot(HaveOccurred())
-
-				err = osFs.Chown(testPath, "root")
-				Expect(err).ToNot(HaveOccurred())
-				testPathStat, err := osFs.Stat(testPath)
-				Expect(err).ToNot(HaveOccurred())
-
-				Expect(testPathStat.Sys().(*syscall.Stat_t).Uid).To(Equal(uint32(0)))
-				Expect(testPathStat.Sys().(*syscall.Stat_t).Gid).To(Equal(uint32(0)))
-			})
-		}
-
-		Context("given an empty owner", func() {
-			It("should return an error", func() {
-				osFs := createOsFs()
-
-				err := osFs.Chown(testPath, "")
-				Expect(err).To(HaveOccurred())
-				Expect(err.Error()).To(ContainSubstring("Failed to lookup user ''"))
-
-			})
-		})
-
-		Context("given a path that does not exist", func() {
-			It("should return an error", func() {
-				osFs := createOsFs()
-
-				err := osFs.Chown("/path-that-does-not-exist", "root")
-				Expect(err).To(HaveOccurred())
-			})
-		})
-
-		Context("given a user that does not exist", func() {
-			It("should return error", func() {
-				osFs := createOsFs()
-
-				err := osFs.Chown(testPath, "garbage-foo")
-				Expect(err).To(HaveOccurred())
-				Expect(err.Error()).To(ContainSubstring("Failed to lookup user 'garbage-foo'"))
-			})
-		})
-
-		Context("given a group that does not exist", func() {
-			It("should return error", func() {
-				osFs := createOsFs()
-
-				err := osFs.Chown(testPath, "root:not-a-group")
-				Expect(err).To(HaveOccurred())
-				Expect(err.Error()).To(ContainSubstring("Failed to lookup group 'not-a-group'"))
-			})
-		})
 	})
 
 	It("chmod", func() {
