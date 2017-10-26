82b7b83cbcb4271c412334d0a5bdbacdcf00553d
Genevieve LEsperance <glesperance@pivotal.io>
2017-10-25 18:15:16 -0700

    Use socks5-proxy dialer when BOSH_ALL_PROXY has ssh+socks5:// schema.
    [#152085398]

--- httpclient/socksify.go
+++ httpclient/socksify.go
@@ -1,9 +1,13 @@
 package httpclient
 
 import (
+	"io/ioutil"
 	"net"
 	"net/url"
 	"os"
+	"strings"
+
+	proxy "github.com/cloudfoundry/socks5-proxy"
 
 	goproxy "golang.org/x/net/proxy"
 )
@@ -16,6 +20,28 @@ func SOCKS5DialFuncFromEnvironment(origDialer DialFunc) DialFunc {
 	allProxy := os.Getenv("BOSH_ALL_PROXY")
 	if len(allProxy) == 0 {
 		return origDialer
+	}
+
+	if strings.HasPrefix(allProxy, "ssh+") && strings.Contains(allProxy, "?private-key=") {
+		allProxy = strings.TrimPrefix(allProxy, "ssh+socks5://")
+
+		proxyBaseURL := strings.Split(allProxy, "?")[0]
+		proxySSHKeyPath := strings.Split(allProxy, "=")[1]
+
+		proxySSHKey, err := ioutil.ReadFile(proxySSHKeyPath)
+		if err != nil {
+			return origDialer
+		}
+
+		socks5Proxy := proxy.NewSocks5Proxy(proxy.NewHostKeyGetter())
+		dialer, err := socks5Proxy.Dialer(string(proxySSHKey), proxyBaseURL)
+		if err != nil {
+			return origDialer
+		}
+
+		return func(network, address string) (net.Conn, error) {
+			return dialer(network, address)
+		}
 	}
 
 	proxyURL, err := url.Parse(allProxy)
