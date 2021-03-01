ac8c81d623fa88b59f5898df9c0db642373db311
Sam Coward <scoward@pivotal.io>
2021-03-01 16:29:55 -0500

    Avoid manipulating GOPATH
    Shouldn't be necessary with go mod
    
    [#177096307](https://www.pivotaltracker.com/story/show/177096307)

--- ci/tasks/bump-deps.sh
+++ ci/tasks/bump-deps.sh
@@ -4,12 +4,7 @@ set -e
 
 git clone bosh-utils bumped-bosh-utils
 
-mkdir -p workspace/src/github.com/cloudfoundry/
-ln -s $PWD/bumped-bosh-utils workspace/src/github.com/cloudfoundry/bosh-utils
-
-export GOPATH=$PWD/workspace
-
-cd workspace/src/github.com/cloudfoundry/bosh-utils
+cd bumped-bosh-utils
 
 go get -u ./...
 go mod tidy
