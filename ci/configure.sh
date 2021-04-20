7ea455556f99f9306cf9efcc5b4768015a40a80b
Maya Rosecrance <mrosecrance@vmware.com>
2021-04-20 11:50:29 -0700

    Add concourse target
    [#177737289](https://www.pivotaltracker.com/story/show/177737289)

--- ci/configure.sh
+++ ci/configure.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-fly -t production set-pipeline -p bosh-utils -c ci/pipeline.yml \
+fly -t "${CONCOURSE_TARGET:-bosh}" set-pipeline -p bosh-utils -c ci/pipeline.yml \
     --load-vars-from <(lpass show -G "bosh-utils concourse secrets" --notes) \
     --load-vars-from <(lpass show --note "bosh:docker-images concourse secrets")
