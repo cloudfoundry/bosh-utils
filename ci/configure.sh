fdb42f4b81b50b3ffd6e392c620125bc7569aea6
Joseph Palermo <jpalermo@pivotal.io>
2021-08-14 13:08:04 -0700

    Move ci to bosh-ecosystem concourse

--- ci/configure.sh
+++ ci/configure.sh
@@ -2,6 +2,8 @@
 
 set -e
 
-fly -t "${CONCOURSE_TARGET:-bosh}" set-pipeline -p bosh-utils -c ci/pipeline.yml \
+lpass ls > /dev/null
+
+fly -t "${CONCOURSE_TARGET:-bosh-ecosystem}" set-pipeline -p bosh-utils -c ci/pipeline.yml \
     --load-vars-from <(lpass show -G "bosh-utils concourse secrets" --notes) \
     --load-vars-from <(lpass show --note "bosh:docker-images concourse secrets")
