7f4a1efe266e1ea0242234dc74ffeea0e7a0d166
Rajan Agaskar <ragaskar@gmail.com>
2021-10-25 16:58:48 -0700

    WIP

--- ci/tasks/bump-deps.sh
+++ ci/tasks/bump-deps.sh
@@ -6,6 +6,11 @@ git clone bosh-utils bumped-bosh-utils
 
 cd bumped-bosh-utils
 
+#intentionally cause an explicit commit if the underlying go version in our compiled Dockerfile changes. 
+#assume the go-dep-bumper and bosh-utils are bumping at the same cadence.
+export GO_MINOR=$(go version | sed 's/go version go1\.\([0-9]\+\)\..*$/\1/g')
+sed -i "s/\(go 1.\)\([0-9]\+\)/\1$GO_MINOR/" go.mod
+
 go get -u ./...
 go mod tidy
 go mod vendor
