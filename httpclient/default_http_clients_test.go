316f9374c8edbc455cff729eb2fcd65f47ececea
Long Nguyen <nguyenlo@vmware.com>
2022-09-08 14:47:42 -0700

    Add function to reset http client dialer func
    The dialer function is created as a var, which means
    if you set PROXY environment variables at runtime
    they will not be respected by the dialer. This function allows
    you to recreate the dialer after you have configured
    the PROXY settings.
    
    Signed-off-by: Joseph Palermo <jpalermo@pivotal.io>
    Co-authored-by: Joseph Palermo <jpalermo@pivotal.io>

--- httpclient/default_http_clients_test.go
+++ httpclient/default_http_clients_test.go
@@ -3,6 +3,8 @@ package httpclient_test
 import (
 	"crypto/tls"
 	"net/http"
+	"os"
+	"reflect"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -68,6 +70,23 @@ var _ = Describe("Default HTTP clients", func() {
 		It("disables HTTP Transport keep-alive (disables HTTP/1.[01] connection reuse)", func() {
 			client := CreateDefaultClientInsecureSkipVerify()
 			Expect(client.Transport.(*http.Transport).DisableKeepAlives).To(Equal(true))
+		})
+	})
+
+	Describe("ResetDialerContext", func() {
+		It("recreates the dialer context to pick up new PROXY config that may have been set during runtime", func() {
+			client := CreateDefaultClient(nil)
+			clientTransport, _ := client.Transport.(*http.Transport)
+
+			originalProxyEnv := os.Getenv("BOSH_ALL_PROXY")
+			os.Setenv("BOSH_ALL_PROXY", "socks5://127.0.0.1:22")
+			ResetDialerContext()
+			os.Setenv("BOSH_ALL_PROXY", originalProxyEnv)
+			clientAfterReset := CreateDefaultClient(nil)
+			clientAfterResetTransport, _ := clientAfterReset.Transport.(*http.Transport)
+			clientDialContextPointer := reflect.ValueOf(clientTransport.DialContext).Pointer()
+			clientAfterResetDialContextPointer := reflect.ValueOf(clientAfterResetTransport.DialContext).Pointer()
+			Expect(clientAfterResetDialContextPointer).ToNot(Equal(clientDialContextPointer))
 		})
 	})
 })
