2f285c37fde4ca1efbf3eeac390758e191329966
Joseph Palermo <jpalermo@pivotal.io>
2023-01-11 11:46:30 -0800

    Update ci pipeline with new credhub entries

--- ci/configure.sh
+++ ci/configure.sh
@@ -4,6 +4,4 @@ set -e
 
 lpass ls > /dev/null
 
-fly -t "${CONCOURSE_TARGET:-bosh-ecosystem}" set-pipeline -p bosh-utils -c ci/pipeline.yml \
-    --load-vars-from <(lpass show -G "bosh-utils concourse secrets" --notes) \
-    --load-vars-from <(lpass show --note "bosh:docker-images concourse secrets")
+fly -t "${CONCOURSE_TARGET:-bosh-ecosystem}" set-pipeline -p bosh-utils -c ci/pipeline.yml
