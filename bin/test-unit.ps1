d56c2747e30607832e77e1e82f749cf5f9a30ae6
rkoster <hi@rkoster.dev>
2024-11-12 11:18:10 +0100

    Conditionally invoke windows or linux unit-test script

---
+++ bin/test-unit.ps1
@@ -0,0 +1,13 @@
+trap {
+  write-error $_
+  exit 1
+}
+
+go.exe version
+
+go.exe run github.com/onsi/ginkgo/v2/ginkgo -r -keep-going --skip-package="vendor"
+if ($LastExitCode -ne 0)
+{
+    Write-Error $_
+    exit 1
+}
