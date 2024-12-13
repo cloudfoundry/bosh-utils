fa97358a4388761caee00b72b6fd88ac4ce2be75
aram price <aram.price@broadcom.com>
2024-12-13 14:06:42 -0800

    Spec: verify multidigest specs can run in parallel

--- main/main_suite_test.go
+++ main/main_suite_test.go
@@ -3,25 +3,27 @@ package main_test
 import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gexec"
 
 	"testing"
-
-	"github.com/onsi/gomega/gexec"
 )
 
 var pathToBoshUtils string
 
 func TestVerifyMultidigestMain(t *testing.T) {
 	RegisterFailHandler(Fail)
-	BeforeSuite(func() {
-		var err error
-		pathToBoshUtils, err = gexec.Build("github.com/cloudfoundry/bosh-utils/main")
-		Ω(err).ShouldNot(HaveOccurred())
-	})
-
-	AfterSuite(func() {
-		gexec.CleanupBuildArtifacts()
-	})
-
 	RunSpecs(t, "Verify Multidigest (main) Suite")
 }
+
+var _ = SynchronizedBeforeSuite(func() []byte {
+	verifyMultidigestBin, err := gexec.Build("github.com/cloudfoundry/bosh-utils/main")
+	Expect(err).NotTo(HaveOccurred())
+
+	return []byte(verifyMultidigestBin)
+}, func(data []byte) {
+	pathToBoshUtils = string(data)
+})
+
+var _ = SynchronizedAfterSuite(func() {}, func() {
+	gexec.CleanupBuildArtifacts()
+})
